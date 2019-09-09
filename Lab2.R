a3fff1f15e34c50170f1b6ee7c99ec0f1c3a7e3d
Luke Boagni <lukeboagni@Lukes-MacBook-Pro.local>
2019-09-09 14:17:06 -0500

    first try

---
+++ Lab2.R
@@ -0,0 +1,19 @@
+#left to right assignment operator
+z=1
+#right to left assignment operator 
+2->y
+#objects with different types
+x=2.5
+w=as.integer(3)
+v='cabbage'
+number=as.factor(1)
+
+#custom operators
+
+
+
+#Create an object that has a sequence of 10 numbers assigned to it
+
+#Create three objects that each have a number assigned to them
+#The code for all three objects must exists a single line in the script
+#Add code to print the values of those three objects
