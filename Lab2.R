21eaf52d7c851344f4f6f270fe810d2fda190650
Luke Boagni <lukeboagni@Lukes-MacBook-Pro.local>
2019-09-11 10:09:23 -0500

    load and initialize

--- Lab2.R
+++ Lab2.R
@@ -1,7 +1,9 @@
 #left to right assignment operator
 z=1
+
 #right to left assignment operator 
 2->y
+
 #objects with different types
 x=2.5
 w=as.integer(3)
@@ -9,11 +11,25 @@ v='cabbage'
 number=as.factor(1)
 
 #custom operators
-
+x%%w #mod function
+x^w #expontential function
+x%/%w #remainder
 
 
 #Create an object that has a sequence of 10 numbers assigned to it
+u=(1:10)
 
 #Create three objects that each have a number assigned to them
 #The code for all three objects must exists a single line in the script
 #Add code to print the values of those three objects
+
+a=1;b=2;c=3
+print(a);print(b);print(c)
+
+#Complete the following series of mathematical operations
+x*y+z #multiplication then addition
+x*(y+z) #parentheses the multiplication
+
+#Inf, -Inf, NaN, NULL and NA
+2^2000
+-2^2000
---
+++ Lab3.R
@@ -0,0 +1,13 @@
+a=c(2:14)
+
+b=c("hello","cat","dog","pig")
+
+b=list(b)
+
+m = matrix(data=1:12, nrow=4, ncol=3, 
+           dimnames = list(c("r1","r2","r3","r4"),
+                           c("c1","c2","c3")))
+
+a.ray = array(data=1:24, dim = c(3,4,2))
+
+df = data.frame(a=c(1,2,3,4), b=c(1,2,3,4))
