e82668cec4c901cb6443cbadb68edd36127379ed
Luke Boagni <lukeboagni@Lukes-MacBook-Pro.local>
2019-09-26 08:48:03 -0500

    test 1 edits

--- Lab2.R
+++ Lab2.R
@@ -1,68 +1,13 @@
-#left to right assignment operator
-z=1
+a=c(2:14)
 
-#right to left assignment operator 
-2->y
+b=c("hello","cat","dog","pig")
 
-#objects with different types
-x=2.5
-w=as.integer(3)
-v='cabbage'
-number=as.factor(1)
+b=list(b)
 
-#custom operators
-x%%w #mod function
-x^w #expontential function
-x%/%w #remainder
+m = matrix(data=1:12, nrow=4, ncol=3, 
+           dimnames = list(c("r1","r2","r3","r4"),
+                           c("c1","c2","c3")))
 
+a.ray = array(data=1:24, dim = c(3,4,2))
 
-#Create an object that has a sequence of 10 numbers assigned to it
-u=(1:10)
-
-#Create three objects that each have a number assigned to them
-#The code for all three objects must exists a single line in the script
-#Add code to print the values of those three objects
-
-a=1;b=2;c=3
-print(a);print(b);print(c)
-
-#Complete the following series of mathematical operations
-x*y+z #multiplication then addition
-x*(y+z) #parentheses the multiplication
-
-#Inf, -Inf, NaN, NULL and NA
-2^2000 #higher number than console can compute
--2^2000 #-inf
-0/0 #nan = Not a number
-m=matrix(data=1:10,nrow = 2,ncol = 3, dimnames = NULL) 
-# null sets statement to false
-
-#custom operator
-'%myop%'=function(a,b){(a*2)+(b*3)}
-
-#animals
-animals=function(x){
-  if(x=="d")
-    "hummingbird"
-  else if(x=="e")
-    'sloth'
-  else if(x=="f")
-    'turtle'
-  else if(x=="g")
-    'whale shark'
-  else 'snek'
-}
-
-#fish data script
-load(fish)
-nrow(fish)
-ncol(fish)
-str(fish)
-#character: used for names, not numbers
-#numeric: numeric data
-#integer: integer data
-#factor - allows you to sort different "levels"
-#POSIXct - date time class
-as.character(fish$area_fac)
-remove(fish$avg.DNE.m)
-
+df = data.frame(a=c(1,2,3,4), b=c(1,2,3,4))
--- Lab3.R
+++ Lab3.R
@@ -1,13 +1,23 @@
-a=c(2:14)
+fish.e = fish[fish$area_fac =="East",]
+e.lat = fish[fish$area_fac == "East",]$parcel.start.lat
+e.lat [6]
+fish_west = fish[fish$area_fac !="East" & fish$area_fac != "Central",]
+fish_lg_patch = fish[fish$parcel.length.m >=20]
+v = c(1,1,0,2,2,3,3,4)
+2%in%v
+c(1,2,3) %in% c(3,2,1)
+v[4]= 6
+v[c(1,3,5)]=c(2,4,6)
 
-b=c("hello","cat","dog","pig")
+n = c(NA, 1:50)
+n.a = is.na(n)
 
-b=list(b)
+names(fish)
+unique(fish$year)
+fish$year[6]=NA
+head(fish$year)
+fish.no.na=fish[!is.na(fish$year)]
+unique(fish.no.na)
 
-m = matrix(data=1:12, nrow=4, ncol=3, 
-           dimnames = list(c("r1","r2","r3","r4"),
-                           c("c1","c2","c3")))
-
-a.ray = array(data=1:24, dim = c(3,4,2))
-
-df = data.frame(a=c(1,2,3,4), b=c(1,2,3,4))
+file = list.files(full.names=TRUE, pattern = ".txt")
+d = read.table(file,skip=10) 
